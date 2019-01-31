6334b9cbf1c541eedbdd483c04a1bcd3c0184290
vagrant <vagrant@vagrant.vm>
2019-01-31 17:51:58 +0000

    quiz #1 attmept 2

--- main.hs
+++ main.hs
@@ -1,4 +1,4 @@
 finalGrade :: [x] -> [y] -> Int
 finalGrade xs ys = case xs of [] -> 0
-finalGrade xs ys = case xs of [x] -> ( (head x)*(head y)/(head y) )
-finalGrade xs ys = case xs of xs -> ( (head x)*(head y)/(head y) ) + (finalGrade (tail x) (tail y) )
+                              [x] -> ( (head x)*(head y)/(head y) )
+                              xs -> ( (head x)*(head y)/(head y) ) + (finalGrade (tail x) (tail y) )
